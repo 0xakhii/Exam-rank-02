0e4b736af726fbed47184843838a884960a9b0fb
cat161 <ojamal@student.1337.ma>
2023-03-20 20:42:45 +0000

    added some exs in level2

---
+++ level2/ft_strdup/ft_strdup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ojamal <ojamal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/20 20:30:54 by ojamal            #+#    #+#             */
+/*   Updated: 2023/03/20 20:34:32 by ojamal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char    *ft_strdup(char *src)
+{
+	int i = 0;
+	char *str;
+	while (src[i])
+		i++;
+	str = malloc(sizeof(char *) * (i + 1));
+	if (!str)
+		return NULL;
+	i = 0;
+	while(src[i])
+	{
+		str[i] = src[i];
+		i++;
+	}
+	str[i] = '\0';
+	return str;
+}
