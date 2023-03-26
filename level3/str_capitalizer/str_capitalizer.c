bfa5802e99b76d302b7389031dd1e62aec8a5eb5
cat161 <ojamal@student.1337.ma>
2023-03-26 21:16:29 +0000

    added str_capitalizer

---
+++ level3/str_capitalizer/str_capitalizer.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   str_capitalizer.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ojamal <ojamal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/26 20:55:28 by ojamal            #+#    #+#             */
+/*   Updated: 2023/03/26 21:16:10 by ojamal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	str_capitalizer(char *str)
+{
+	int i = 0;
+	if (str[i] >= 'a' && 'z' >= str[i])
+		str[i] -= 32;
+	write(1, &str[i], 1);
+	while (str[++i])
+	{
+		if (str[i] >= 'A' && 'Z' >= str[i])
+			str[i] += 32;
+		if ((str[i] >= 'a' && 'z' >= str[i]) && (str[i - 1] == ' ' || \
+		str[i - 1] == '\t'))
+			str[i] -= 32;
+		write(1, &str[i], 1);
+	}
+	write(1, "\n", 1);
+}
+
+int main(int ac, char **av)
+{
+	if (ac > 1)
+	{
+		int j = 0;
+		while (++j < ac)
+			str_capitalizer(av[j]);
+	}
+	else
+		write(1, "\n", 1);
+}
