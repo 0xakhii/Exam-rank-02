7ab8fdba706711488d4067cf912ee6447918b788
cat161 <ojamal@student.1337.ma>
2023-04-05 22:20:09 +0000

    updated print_hex

--- level3/print_hex/print_hex.c
+++ level3/print_hex/print_hex.c
@@ -6,35 +6,32 @@
 /*   By: ojamal <ojamal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 21:25:41 by ojamal            #+#    #+#             */
-/*   Updated: 2023/03/24 21:33:04 by ojamal           ###   ########.fr       */
+/*   Updated: 2023/04/05 22:18:27 by ojamal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_atoi(char *str)
+int mini_atoi(char *str)
 {
-	int i = 0;
 	int res = 0;
-	while (str[i])
-	{
+	int i = -1;
+	while(str[++i])
 		res = res * 10 + str[i] - '0';
-		i++;
-	}
 	return res;
 }
 
-void	print_hex(int nbr)
+void print_hex(int n)
 {
-	char str[] = "0123456789abcdef";
-	if (nbr >= 16)
-		print_hex(nbr / 16);
-	write(1, &str[nbr % 16], 1);
+	char *str = "0123456789abcdef";
+	if (n >= 16)
+		print_hex(n / 16);
+	write(1, &str[n % 16], 1);
 }
 
 int main(int ac, char **av)
 {
 	if (ac == 2)
-		print_hex(ft_atoi(av[1]));
+		print_hex(mini_atoi(av[1]));
 	write(1, "\n", 1);
 }
