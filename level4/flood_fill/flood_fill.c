8c8b1ff1d7b43dacf157f3eac770397d9b4dad85
cat161 <ojamal@student.1337.ma>
2023-04-09 03:13:13 +0000

    added flood_fill

--- level4/flood_fill/flood_fill.c
+++ level4/flood_fill/flood_fill.c
@@ -6,7 +6,26 @@
 /*   By: ojamal <ojamal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 03:09:39 by ojamal            #+#    #+#             */
-/*   Updated: 2023/04/09 03:09:41 by ojamal           ###   ########.fr       */
+/*   Updated: 2023/04/09 03:12:48 by ojamal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "flood_fill.h"
+
+
+void	fill(char **tab, t_point size, t_point cur, char to_fill)
+{
+	if (cur.y < 0 || cur.y >= size.y || cur.x < 0 || cur.x >= size.x
+		|| tab[cur.y][cur.x] != to_fill)
+		return;
+	tab[cur.y][cur.x] = 'F';
+	fill(tab, size, (t_point){cur.x - 1, cur.y}, to_fill);
+	fill(tab, size, (t_point){cur.x + 1, cur.y}, to_fill);
+	fill(tab, size, (t_point){cur.x, cur.y - 1}, to_fill);
+	fill(tab, size, (t_point){cur.x, cur.y + 1}, to_fill);
+}
+
+void	flood_fill(char **tab, t_point size, t_point begin)
+{
+	fill(tab, size, begin, tab[begin.y][begin.x]);
+}
